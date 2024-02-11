846600410357a174bca1ed1673d91823b5d0d851
ruzny <roony0782045336@gmail.com>
2024-02-11 21:54:05 +0530

    removed https certificates

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,13 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import * as fs from 'fs';
 
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './config/all-exceptions.filter';
 import corsOptions from './config/cors-options';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./src/cert/key.pem'),
-    cert: fs.readFileSync('./src/cert/cert.pem'),
-  };
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
